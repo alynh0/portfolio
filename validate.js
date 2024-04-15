c5c950ccc521e794ca155a5b44bf1f1ea7ae6093
Alysson <ajsouzapc@gmail.com>
2024-04-15 17:32:35 -0300

    feat: Validação do formulário de contato

--- validate.js
+++ validate.js
@@ -1,1 +1,31 @@
-//Seu JavaScript de validação aqui
+// Arquivo JavaScript para validação de formulários
+
+let inputNome = document.getElementById("nome");
+// TODO: melhorar validação de e-mail utilizando expressões regulares.
+let inputEmail = document.getElementById("email");
+let inputAssunto = document.getElementById("assunto");
+let inputMensagem = document.getElementById("mensagem");
+
+function validarFormulario() {
+  if (inputNome.value == "") {
+    alert('O campo "Nome" não pode ser vazio');
+    return false;
+  }
+
+  if (inputEmail.value == "") {
+    alert('O campo "Email" não pode ser vazio');
+    return false;
+  }
+
+  if (inputAssunto.value == "") {
+    alert('O campo "Assunto" não pode ser vazio');
+    return false;
+  }
+
+  if (inputMensagem.value == "") {
+    alert('O campo "Mensagem" não pode ser vazio');
+    return false;
+  }
+
+  return true;
+}
